6ab62d4e21c9ef5835fb6dea8f321fddd33a2f69
jayro rene salgado alvarado <jsalgado@nearsoft.com>
2017-06-09 15:35:52 -0700

    Develop (#2)
    * inti api
    
    * change dockerfile port
    
    * decople docker-compose for development environment
    
    * docker run documentation
    
    * add new home route
    
    * connect to db and enable routes

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
-var express = require('express');
-var app = express();
-var _ = require('lodash')
+const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://db/kittens');
+
+const voteSchema = new mongoose.Schema({
+  kittenId: String,
+  votes: Number
+})
+
+const Votes = mongoose.model('Votes', voteSchema);
+
+const app = express();
+
+app.use('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    vote: '/vote',
+    results: '/results',
+  })
+})
 
 app.get('/vote', function (req, res) {
-  res.send('vote');
-});
+  const kittenId = req.query.kittenId
+  if (!kittenId) return res.status(500).send('error')
 
-app.get('/cutekitten', function (req, res) {
-  console.log({lodash:_})
-  res.send('kitten');
-});
+  Votes.findOneAndUpdate(
+    { kittenId },
+    { '$inc': { votes: 1 } },
+    { upsert: true, new: true },
+    (err, vote) => {
+      if (err) return res.status(500).send('error')
+      res.json(vote);
+    })
+})
 
-app.listen(3100);
+app.get('/results', function (req, res) {
+  Votes.find({}, (err, votes) => {
+    if (err) {
+      res.status(500).send('error');
+    } else {
+      res.json(votes)
+    }
+  })
+})
+
+app.listen(3100)
+
